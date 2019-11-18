842f3ba55d1ad07babf426e1cd46feb37d417573
snufka <52801250+snufka@users.noreply.github.com>
2019-11-18 21:12:40 +0100

    commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt({
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "username"
+    },
+        {
+            type: "input",
+            name: "location",
+            message: "Where are you from?"
+        },
+        {
+            type: "checkbox",
+            name: "color",
+            message: "What is your favorite color?",
+            choosies: ["green", "yellow", "red"]
+        })
+    .then(function ({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+        axios.get(queryUrl).then(function (res) {
+            const repoNames = res.data.map(function (repo) {
+                return repo.name;
+            });
+
+            const repoNamesStr = repoNames.join("\n");
+
+            fs.writeFile("repos.txt", repoNamesStr, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Saved ${repoNames.length} repos`);
+            });
+        });
+    });
